f0034674b833265531670e780c6ed493b5b0ae60
dw6ja <wudiyangabc@hotmail.com>
2019-02-26 07:42:46 +0000

    create data.R and start reading original data

---
+++ hw3/data.R
@@ -0,0 +1,12 @@
+############################
+# Diyang Wu                #
+# Biostats M280            #
+# Homework 3 (Data Process)#
+############################
+
+#read original data
+library("tidyverse")
+la_payroll <- read.csv("/home/m280data/la_payroll/City_Employee_Payroll.csv")
+head(la_payroll)
+
+#process data for Question 1
