e2e34be8c73923438761d64c15c9db8384568c8e
dw6ja <wudiyangabc@hotmail.com>
2019-02-26 08:49:09 +0000

    finish problem1 part1

--- hw3/data.R
+++ hw3/data.R
@@ -8,5 +8,17 @@
 library("tidyverse")
 la_payroll <- read.csv("/home/m280data/la_payroll/City_Employee_Payroll.csv")
 head(la_payroll)
+names(la_payroll)
 
-#process data for Question 1
+#process data for Question 1 part 1
+q1p1 <- la_payroll %>% 
+  select(Year, Total.Payments, Base.Pay, Overtime.Pay, 
+         Other.Pay..Payroll.Explorer.)  %>% 
+  group_by(Year) %>%
+  summarise(
+    Total_Payments = sum(Total.Payments, na.rm = TRUE), 
+    Base_Pay = sum(Base.Pay, na.rm = TRUE), 
+    Overtime_Pay = sum(Overtime.Pay, na.rm = TRUE), 
+    Other_Pay = sum(Other.Pay..Payroll.Explorer., na.rm = TRUE)
+  )
+q1p1
